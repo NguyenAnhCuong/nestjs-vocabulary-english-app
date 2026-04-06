a9336a9fc22c4057097b16deb39602cb929db360
cuong <cuong1606x@gmail.com>
2026-04-06 16:32:29 +0700

    fix update user

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,17 +1,35 @@
-import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  MinLength,
+} from 'class-validator';
 
 export class CreateUserDto {
   @IsEmail({}, { message: 'Email không đúng định dạng' })
   @IsNotEmpty({ message: 'Email không được để trống' })
-  email: string;
+  email!: string;
 
   @IsNotEmpty({ message: 'Mật khẩu không được để trống' })
   @MinLength(6, { message: 'Mật khẩu phải có ít nhất 6 ký tự' })
-  password: string;
+  password!: string;
 
+  @IsOptional()
+  @IsString()
   name?: string;
+
+  @IsOptional()
   role?: string;
+
+  @IsOptional()
   gender?: string;
+
+  @IsOptional()
+  @IsNumber()
   age?: number;
+
+  @IsOptional()
   address?: string;
 }
--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -3,6 +3,8 @@ import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsOptional()
+  @IsString()
   name?: string;
 
   @IsOptional()
